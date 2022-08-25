dfb9a8fe0767a6f64773ae8ec3f7cb99e42bfa00
Mateus Castro da Cunha <mateus.mc87@gmail.com>
2022-08-25 18:15:29 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 // ----- Datas no dashboard ----------------------
-let dias = document.querySelector(".counter-dias");
-let horas = document.querySelector(".counter-horas");
-let minutos = document.querySelector(".counter-minutos");
-let segundos = document.querySelector(".counter-segundos");
+let dias = document.querySelector(".get-days");
+let horas = document.querySelector(".get-hours");
+let minutos = document.querySelector(".get-minutes");
+let segundos = document.querySelector(".get-seconds");
+
+
 
 // ---------- Input dados usuário -------------------------
 let dataUser = document.getElementById("dataUser");
-console.log(dataUser);
+
+let i = 0;
+
 function getUserDate() {
-  setInterval(console.log(dataUser), 1000);
+  i++;
+  
+
+  dias.innerHTML = i;
+  horas.innerHTML = i;
+  minutos.innerHTML = i;
+  segundos.innerHTML = i; 
+
+  /*let goalDate = new Date(dataUser.value);
+  let currentDate = new Date('dd-MM-yyyyy');
+
+  // Calculo do tempo restante----Current date to Goal Date-----
+
+  let remainingSeconds = ((currentDate - goalDate)* -1)/ 1000;
+  let remainingMinutes = remainingSeconds / 60;
+  let remainingHours = remainingMinutes / 60;
+  let remainingDays = remainingHours / 24; 
+  console.log('Data desejada:', goalDate);
+  console.log('Data atual:', currentDate);
+  console.log('Datas sendo subtraidas:', currentDate - goalDate)
+
+  console.log('Dias restantes', remainingDays)
+  console.log('Horas restantes', remainingHours)
+  console.log('Minutos restantes', remainingMinutes)
+  console.log('Segundos restantes', remainingSeconds)*/
 }
+setInterval(getUserDate, 1000)
